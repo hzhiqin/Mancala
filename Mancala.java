7d3d4177452fc5d970f78eb6af700ac26aaa1bc5
hzhiqin <huang_zq@yahoo.com>
2019-01-14 23:50:40 +0800

    Add files via upload

---
+++ Mancala.java
@@ -0,0 +1,59 @@
+/**
+ * Created by zhiqinhuang on 4/9/17.
+ */
+public class Mancala {
+    public static void main(String[] args) {
+        BoardState.PITSNUMBER = 9;//Integer.parseInt(args[0]);
+        BoardState boardState = new BoardState();
+        MancalaAI playerA = new MancalaAI(false);
+        GreedyAI playerB = new GreedyAI();
+        Result r = new Result();
+        int AIMove = 1 + (int) (Math.random() * BoardState.PITSNUMBER);
+        Board.printBoard(boardState);
+        for (; ; ) {
+
+            if (!boardState.player) {
+                playerA.init(boardState);
+                playerA.buildTree(playerA.searchTree.get(0));
+                AIMove = playerA.think();
+
+                System.out.println();
+                System.out.println("playerA move position: " + AIMove);
+                //if(AIMove==0) break;
+            } else {
+                r = playerB.Greedy(boardState);
+                System.out.println();
+                System.out.println("playerB move position: " + r.pit + " " + r.max);
+            }
+            if (!boardState.player)
+                boardState.updateMove(AIMove, 'a', 0);
+            else
+                boardState.updateMove(r.pit, 'b', 0);
+            if (Referee.isEnd(boardState)) {
+                showResult(boardState);
+                break;
+            }
+            Board.printBoard(boardState);
+        }
+    }
+
+    /**
+     * print game result on command line
+     *
+     * @param boardState
+     */
+    static void showResult(BoardState boardState) {
+        int[] result = Referee.judge(boardState);
+        System.out.println();
+        if (result[0] > result[1]) {
+            System.out.println("player A won");
+        }
+        if (result[0] < result[1]) {
+            System.out.println("player B won");
+        }
+        if (result[0] == result[1]) {
+            System.out.println("draw");
+        }
+        System.out.println("    " + result[0] + ":" + result[1]);
+    }
+}
